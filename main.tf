64cd89645035856ae979d2c0bbbac19179c22acc
swedev99 <swedevops99@gmail.com>
2023-05-10 12:38:56 +0530

    code dry

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
   value = "hello world"
+  type  = "SecureString"
+  key_id = "cc793974-5eaf-4f40-9c9f-92dd8dfdf28d"
 }
