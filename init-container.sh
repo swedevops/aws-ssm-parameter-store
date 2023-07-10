6852b36430f07c2ea9ad7a73a76212d9b92d0d90
swedev99 <swedevops99@gmail.com>
2023-07-10 14:39:43 +0530

    Jenkins pipeline concepts

--- init-container.sh
+++ init-container.sh
@@ -1,1 +1,6 @@
-aws ssm get-parameters --names dev.payment.amqp_host | jq .Parameters[].Value|sed -e 's/"//g'
+for param in ${PARAMS}; do
+  VAR=$(echo $param | awk -F . '{print $NF}' | tr '[:lower:]' '[:upper:]' )
+  VAL=$(aws ssm get-parameters  --names $param --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  echo export $VAR=\"$VAL\"
+  echo export $VAR=\"$VAL\" >>/params/params
+done
